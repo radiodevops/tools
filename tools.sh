cccb9cf5a2464344208d1f9886aad78df600b9c8
radiodevops <144582815+radiodevops@users.noreply.github.com>
2024-10-15 19:22:52 +0500

    Creates tools.sh

---
+++ tools.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+FONT=JetBrainsMono.zip
+K9S=k9s_linux_amd64.rpm
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/${FONT}
+sudo unzip JetBrainsMono.zip -d /usr/share/fonts/
+fc-cache -fv
+rm -rf $FONT
+
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/${K9S}
+sudo yum install ./${K9S} -y
+rm -rf $K9S
+
+mkdir -p ~/.config
+sudo curl -sS https://starship.rs/install.sh | sh
+
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+starship preset nerd-font-symbols -o ~/.config/starship.toml
+rm -rf install.sh
+
+
+sudo hostnamectl set-hostname dev
+source ~/.bash_profile
